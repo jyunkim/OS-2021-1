fcc793b48c4fa165acc80c9f769687e3470bbe34
jyunkim <jyunkim13@gmail.com>
2021-06-03 15:28:53 +0900

    test: Add input files

--- assignment3/project3.cpp
+++ assignment3/project3.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// ./project3
+// ./project3 -dir=/home/jihyun/OS-2021-1/assignment3
+// ./project3 -page=lru -dir=/home/jihyun/OS-2021-1/assignment3
 int main() {
     printf("hello");
     return 0;
