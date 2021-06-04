b1c25926070cfbdb84c8165efb25f22ec889f101
jyunkim <jyunkim13@gmail.com>
2021-06-04 19:50:51 +0900

    feat: Add checking sleep, IO done and creating process functions

--- assignment3/project3.cpp
+++ assignment3/project3.cpp
@@ -3,235 +3,23 @@
 #include <string>
 #include <fstream>
 #include <deque>
+#include <list>
 
 using namespace std;
-
-
-// void round_robin()
-// {
-// 	printf("Time Quantum for Queue1 is 4\n");
-// 	for(i=0;i<j;i++)
-// 	{
-// 		temp[i]=burst_time1[i];
-// 	} 
-// 	printf("\nProcess ID\tBurst Time\t Turnaround Time\t Waiting Time\n");
-// 	x=j;
-//     for(i=0,total=0;x!=0;) 
-//     { 
-//     	if(temp[i]<=4&&temp[i]>0) 
-//         {
-// 			printf("\nProcess[%d] of Queue1 is running for %d units",i+1,temp[i]); 
-//             total=total+temp[i]; 
-//             temp[i]=0; 
-//             counter=1; 
-//         } 
-//         else if(temp[i]>0) 
-//         {
-// 			printf("\nProcess[%d] of Queue1 is running for 4 units",i+1); 
-//             temp[i]=temp[i]-4; 
-//             total=total+4; 
-//         } 
-//         if(temp[i]==0&&counter==1) 
-//         { 
-//             x--; 
-//             printf("\nProcess[%d]\t%d\t%d\t%d",i+1,burst_time1[i],total-arrival_time1[i],total-arrival_time1[i]-burst_time1[i]);
-//             avg_waiting_time1=avg_waiting_time1+total-arrival_time1[i]-burst_time1[i]; 
-//             avg_turnaround_time1=avg_turnaround_time1+total-arrival_time1[i]; 
-//             counter = 0; 
-//         } 
-//         if(i==j-1) 
-//         {
-//             i=0; 
-//         }
-//         else if(arrival_time1[i+1]<=total) 
-//         {
-//             i++;
-//         }
-//         else 
-//         {
-//             i=0;
-//         }
-//     } 
-//     avg_waiting_time1=avg_waiting_time1/j;
-//     avg_turnaround_time1=avg_turnaround_time1/j;
-//     printf("\nAverage Waiting Time:%f",avg_waiting_time1); 
-//     printf("\nAverage Turnaround Time:%f\n",avg_turnaround_time1); 
-// }
-
-
-// void priority()
-// {
-// 	for(i=0;i<k;i++)
-//     {
-//         position=i;
-//         for(q=i+1;q<k;q++)
-//         {
-//             if(priority2[q]<priority2[position])
-//             {
-//                 position=q;
-//             }
-//         }
-//         temp1=priority2[i];
-//         priority2[i]=priority2[position];
-//         priority2[position]=temp1; 
-        
-//         temp1=burst_time2[i];
-//         burst_time2[i]=burst_time2[position];
-//         burst_time2[position]=temp1;
-        
-//         temp1=process2[i];
-//         process2[i]=process2[position];
-//         process2[position]=temp1;
-//     }
-//     waiting_time2[0]=0;
-//     for(i=1;i<k;i++)
-//     {
-//         waiting_time2[i]=0;
-//         for(q=0;q<i;q++)
-//         {
-//             waiting_time2[i]=waiting_time2[i]+burst_time2[j];
-//         }
-//         sum=sum+waiting_time2[i];
-//     }
-//     avg_waiting_time2=sum/k;
-//     sum=0;
-//     printf("\nProcess ID\t\tBurst Time\t Waiting Time\t Turnaround Time\n");
-//     for(i=0;i<k;i++)
-//     {
-//     	turnaround_time2[i]=burst_time2[i]+waiting_time2[i];
-//         sum=sum+turnaround_time2[i];
-//         printf("\nProcess[%d]\t\t%d\t\t %d\t\t %d\n",process2[i],burst_time2[i],waiting_time2[i],turnaround_time2[i]);
-//     }
-//     avg_turnaround_time2=sum/k;
-//     printf("\nAverage Waiting Time:\t%f",avg_waiting_time2);
-//     printf("\nAverage Turnaround Time:\t%f\n",avg_turnaround_time2);
-    
-//     for(i=0;i<k;i++)
-//     {
-//     	while(burst_time2[i]!=0)
-//     	{
-//     		if(burst_time2[i]>10)
-//     		{
-// 				printf("\nProcess[%d] of Queue2 is running for 10 units",i+1);
-// 				burst_time2[i]=burst_time2[i]-10;
-// 			}
-// 			else if(burst_time2[i]<=10)
-// 			{
-// 				printf("\nProcess[%d] of Queue2 is running for %d units",i+1,burst_time2[i]);
-// 				burst_time2[i]=0;
-// 			}
-// 		}
-// 	}
-
-
-// }
-
-
-// void fcfs()
-// {
-// 	waiting_time3[0] = 0;   
-//     for(i=1;i<l;i++)
-//     {
-//         waiting_time3[i] = 0;
-//         for(p=0;p<l;p++)
-//         {
-//             waiting_time3[i]=waiting_time3[i]+burst_time3[p];
-//         }
-//     }
-//     printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time\n");
-//     for(i=0;i<l;i++)
-//     {
-//         turnaround_time3[i]=burst_time3[i]+waiting_time3[i];
-//         avg_waiting_time3=avg_waiting_time3+waiting_time3[i];
-//         avg_turnaround_time3=avg_turnaround_time3+turnaround_time3[i];
-//         printf("\nProcess[%d]\t\t%d\t\t%d\t\t%d\n",i+1,burst_time3[i],waiting_time3[i],turnaround_time3[i]);
-//     }
-//     avg_waiting_time3=avg_waiting_time3/l;
-//     avg_turnaround_time3=avg_turnaround_time3/l;
-//     printf("\nAverage Waiting Time=%f",avg_waiting_time3);
-//     printf("\nAverage Turnaround Time=%f",avg_turnaround_time3);
-//     for(i=0;i<l;i++)
-//     {
-//     	while(burst_time3[i]!=0)
-//     	{
-//     		if(burst_time3[i]>10)
-//     		{
-// 				printf("\nProcess[%d] of Queue3 is running for 10 units",i+1);
-// 				burst_time3[i]=burst_time3[i]-10;
-// 			}
-// 			else if(burst_time3[i]<=10)
-// 			{
-// 				printf("\nProcess[%d] of Queue2 is running for %d units",i+1,burst_time3[i]);
-// 				burst_time3[i]=0;
-// 			}
-// 		}
-// 	}
-// }
-
-
-// void round_robin1()
-// {
-// 	printf("Time Quantum between the 3 queues is 10\n");
-// 	for(i=1;i<Total;i=i+10)
-// 	{
-// 		if(t1>10)
-// 		{
-// 			printf("Queue1 is running for 10 units\n");
-// 			t1=t1-10;
-// 		}
-// 		else if(t1<=10&&t1!=0)
-// 		{
-// 			printf("Queue1 is running for %d units\n",t1);
-// 			t1=0;
-// 		}
-// 		if(t2>10)
-// 		{
-// 			printf("Queue2 is running for 10 units\n");
-// 			t2=t2-10;
-// 		}
-// 		else if(t2<=10&&t2!=0)
-// 		{
-// 			printf("Queue2 is running for %d units\n",t2);
-// 			t2=0;
-// 		}
-// 		if(t3>10)
-// 		{
-// 			printf("Queue3 is running for 10 units\n");
-// 			t3=t3-10;
-// 		}
-// 		else if(t3<=10&&t3!=0)
-// 		{
-// 			printf("Queue3 is running for %d units\n",t3);
-// 			t3=0;
-// 		}
-// 	}
-// }
-
-// int arrival_time1[30],arrival_time2[30],priority2[30],process2[30],arrival_time3[30];
-// int burst_time1[30],burst_time2[30],burst_time3[30];
-
-// int Total=0,t1=0,t2=0,t3=0;
-
-// int n,i,at[30],bt[30],pr[30],j=0,k=0,l=0;
-
-// int total,x,temp[30],counter=0;
-// float avg_waiting_time1=0.0,avg_turnaround_time1=0.0;
-
-// int p,waiting_time3[30],turnaround_time3[30];
-// float avg_waiting_time3=0.0,avg_turnaround_time3=0.0;
-
-// int position,q,temp1,sum=0,waiting_time2[30],turnaround_time2[30];
-// float avg_waiting_time2,avg_turnaround_time2;
 
 
 // 실행 작업
 class Process {
 public:
+    // input으로 받는 정보들
 	int start_cycle;
 	string name;
 	int priority;
 	int pid;
-    deque<pair<int, int>> instructions;
+    
+    deque<pair<int, int>> instructions;  // 작업 별 명령어 리스트
+    int current_instruction;  // 현재 수행 중인 명령어 index
+    int sleep_time;  // 남은 sleep time
 
 	Process() {}
 
@@ -276,6 +64,104 @@ public:
 		pid = pid_;
 	}
 };
+
+
+// Sleep된 프로세스의 종료 여부 검사
+void checkSleepOver(deque<Process> *run_queue, list<Process> *sleep_list) {
+    list<Process>::iterator iter;
+
+    for(iter = sleep_list->begin(); iter!= sleep_list->end(); iter++) {
+        Process process = *iter;
+        process.sleep_time--;
+        if(process.sleep_time == 0) {
+            run_queue->push_back(process);
+            sleep_list->erase(iter);
+            return;
+        }
+    }    
+    return;
+}
+
+
+// IO 작업 시행 여부 검사
+void checkIO(deque<Process> *run_queue, deque<IO> *ios, list<Process> *iowait_list, int cycle) {
+    int count = 0;
+    list<Process>::iterator iter;
+
+    // 같은 time에 여러 작업이 들어올 수 있으므로 순회
+    for(int i = 0; i < ios->size(); i++) {
+        IO io = ios->at(i);
+        if(io.start_cycle == cycle) {
+            for(iter = iowait_list->begin(); iter!= iowait_list->end(); iter++) {
+                Process process = *iter;
+                if(io.pid == process.pid) {
+                    run_queue->push_back(process);
+                    iowait_list->erase(iter);
+                    count++;
+                    break;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < count; i++) {
+        ios->pop_front();
+    }
+    return;
+}
+
+
+// 프로세스 생성 작업 시행
+void create_process(deque<Process> *run_queue, deque<Process> *processes, int cycle) {
+    int count = 0;
+
+    // 같은 time에 여러 작업이 들어올 수 있으므로 순회
+    for(int i = 0; i < processes->size(); i++) {
+        Process process = processes->at(i);
+        if(process.start_cycle == cycle) {
+            run_queue->push_back(process);
+            count++;
+        }
+    }
+
+    for(int i = 0; i < count; i++) {
+        processes->pop_front();
+    }
+    return;
+}
+
+
+void executeInstruction(Process process, int cur) {
+    int opcode = process.instructions[cur].first;
+    int arg = process.instructions[cur].second;
+
+    // Memory allocation
+    if(opcode == 0) {
+        // cmd_malloc(currentCpuTask, all_pages, runCmd[1], aid);
+    }
+    // Memory access
+    else if(opcode == 1) {
+        // cmd_memAccess(currentCpuTask, runningTask, physicalMem, physicalNum, runCmd[1], currentCycle, page_opt, page_fault);
+    }
+    // Memory release
+    else if(opcode == 2) {
+        // cmd_memFree(currentCpuTask, runCmd[1], physicalMem, physicalNum);
+    }
+    // Non-memory instruction
+    else if(opcode == 3) {
+        // currentCpuTask->flagToEmpty = false;
+        // currentCpuTask->flagToComplete = true;
+    }
+    // Sleep
+    else if(opcode == 4) {
+        // cmd_sleep(currentCpuTask, runCmd[1], sleepList, sched_opt);
+        
+    }
+    // IO wait
+    else if(opcode == 5) {
+        // cmd_ioWait(currentCpuTask, ioWaitList, sched_opt);
+    }
+}
 
 
 // ./project3 -page=lru -dir=/home/jihyun/OS-2021-1/assignment3
@@ -348,8 +234,8 @@ int main(int argc, char *argv[]) {
     deque<Process> run_queue7;
     deque<Process> run_queue8;
     deque<Process> run_queue9;
-    deque<Process> sleep_list;
-    deque<Process> iowait_list;
+    list<Process> sleep_list;
+    list<Process> iowait_list;
 
     // 출력 파일
     string schedule_file = dir + "/scheduler.txt";
@@ -365,38 +251,38 @@ int main(int argc, char *argv[]) {
 
     //작업 수행
     while(process_num > 0) {
-        Process process = processes.front();
+        
 
-        if(process.start_cycle == cycle) {
-            if(process.priority == 0) {
-                run_queue0.push_back(process);
+        if(running_process.start_cycle == cycle) {
+            if(running_process.priority == 0) {
+                run_queue0.push_back(running_process);
             }
-            else if(process.priority == 1) {
-                run_queue1.push_back(process);
+            else if(running_process.priority == 1) {
+                run_queue1.push_back(running_process);
             }
-            else if(process.priority == 2) {
-                run_queue2.push_back(process);
+            else if(running_process.priority == 2) {
+                run_queue2.push_back(running_process);
             }
-            else if(process.priority == 3) {
-                run_queue3.push_back(process);
+            else if(running_process.priority == 3) {
+                run_queue3.push_back(running_process);
             }
-            else if(process.priority == 4) {
-                run_queue4.push_back(process);
+            else if(running_process.priority == 4) {
+                run_queue4.push_back(running_process);
             }
-            else if(process.priority == 5) {
-                run_queue5.push_back(process);
+            else if(running_process.priority == 5) {
+                run_queue5.push_back(running_process);
             }
-            else if(process.priority == 6) {
-                run_queue6.push_back(process);
+            else if(running_process.priority == 6) {
+                run_queue6.push_back(running_process);
             }
-            else if(process.priority == 7) {
-                run_queue7.push_back(process);
+            else if(running_process.priority == 7) {
+                run_queue7.push_back(running_process);
             }
-            else if(process.priority == 8) {
-                run_queue8.push_back(process);
+            else if(running_process.priority == 8) {
+                run_queue8.push_back(running_process);
             }
-            else if(process.priority == 9) {
-                run_queue9.push_back(process);
+            else if(running_process.priority == 9) {
+                run_queue9.push_back(running_process);
             }
         }
         cycle++;
